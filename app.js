11ab6aede6d3191cc37409bbaf8e47a2623b7e39
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-02-28 22:29:08 +0600

    Store pallets to library

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const adjustButton = document.querySelectorAll(".adjust")
 const lockButton = document.querySelectorAll(".lock")
 const closeAdjustments = document.querySelectorAll(".close-adjustment")
 const sliderContainers = document.querySelectorAll(".sliders")
+const saveBtn = document.querySelector(".save")
+const submitSave = document.querySelector(".submit-save")
+const closeSave = document.querySelector(".close-save")
+const saveContainer = document.querySelector(".save-container")
+const saveInput = document.querySelector(".save-container input")
+const libraryContainer = document.querySelector(".library-container")
+const libraryBtn = document.querySelector(".library")
+const closeLibraryBtn = document.querySelector(".close-library")
 let initialColors
+let savedPalettes = []
 
 
 //Event listeners:
@@ -62,6 +71,23 @@ lockButton.forEach((button, index) => {
 		lockLayer(e, index);
 	})
 })
+
+//Information is stored in localStorage
+//Open pallet confirm option
+saveBtn.addEventListener("click", openPalette);
+
+//Close pallet
+closeSave.addEventListener("click", closePalette);
+
+//Submit to store 
+submitSave.addEventListener("click", savePalette);
+
+//Check library
+libraryBtn.addEventListener("click", openLibrary);
+
+//Close library
+closeLibraryBtn.addEventListener("click", closeLibrary);
+
 
 //Functions:
 //Get a random hex value
@@ -248,5 +274,194 @@ function lockLayer(e, index) {
 	}
 }
 
+
+function openPalette(e) {
+	const popup = saveContainer.children[0]
+	saveContainer.classList.add("active")
+	popup.classList.add("active")
+}
+
+function closePalette(e) {
+	const popup = saveContainer.children[0]
+	saveContainer.classList.remove("active")
+	popup.classList.add("remove")
+}
+
+
+function savePalette(e) {
+	saveContainer.classList.remove("active")
+	popup.classList.remove("active")
+	const name = saveInput.value
+	const colors = []
+
+	currentHexes.forEach(hex => {
+		colors.push(hex.innerText)
+	})
+
+	let paletteNr
+
+	const paletteObjects = JSON.parse(localStorage.getItem("palettes"))
+
+	if (paletteObjects) {
+		paletteNr = paletteObjects.length
+	} else {
+		paletteNr = savedPalettes.length
+	}
+
+	const paletteObj = {
+		name,
+		colors,
+		nr: paletteNr
+	}
+
+	savedPalettes.push(paletteObj)
+
+	//Save to localStorage
+	savetoLocal(paletteObj)
+	saveInput.value = ""
+
+	//Generate the palette for Library
+	const palette = document.createElement("div")
+
+	palette.classList.add("custom-palette")
+
+	const title = document.createElement("h4")
+
+	title.innerText = paletteObj.name
+
+	const preview = document.createElement("div")
+
+	preview.classList.add("small-preview")
+
+	paletteObj.colors.forEach(smallColor => {
+		const smallDiv = document.createElement("div")
+		smallDiv.style.backgroundColor = smallColor;
+		preview.appendChild(smallDiv);
+	})
+
+	const paletteBtn = document.createElement("button")
+
+	paletteBtn.classList.add("pick-palette-btn")
+	paletteBtn.classList.add(paletteObj.nr)
+	paletteBtn.innerText = "Select"
+
+	//Attach event to the btn
+	paletteBtn.addEventListener("click", e => {
+		closeLibrary()
+
+		const paletteIndex = e.target.classList[1]
+
+		console.log(paletteIndex)
+
+		initialColors = []
+
+		savedPalettes[paletteIndex].colors.forEach((color, index) => {
+			initialColors.push(color)
+			colorDivs[index].style.backgroundColor = color
+
+			const text = colorDivs[index].children[0]
+
+			checkTextContrast(color, text)
+			updateHexText(index)
+		})
+		resetInput()
+	})
+
+	//Append to Library
+	palette.appendChild(title)
+	palette.appendChild(preview)
+	palette.appendChild(paletteBtn)
+	libraryContainer.children[0].appendChild(palette)
+}
+
+function savetoLocal(paletteObj) {
+	let localPalettes
+	if (localStorage.getItem("palettes") === null) {
+		localPalettes = []
+	} else {
+		localPalettes = JSON.parse(localStorage.getItem("palettes"))
+	}
+	localPalettes.push(paletteObj)
+	localStorage.setItem("palettes", JSON.stringify(localPalettes))
+}
+
+
+function openLibrary() {
+	const popup = libraryContainer.children[0]
+	libraryContainer.classList.add("active")
+	popup.classList.add("active")
+}
+
+function closeLibrary() {
+	const popup = libraryContainer.children[0]
+	libraryContainer.classList.remove("active")
+	popup.classList.remove("active")
+}
+
+function getLocal() {
+	if (localStorage.getItem("palettes") === null) {
+		//Local Palettes
+		localPalettes = []
+	} else {
+		const paletteObjects = JSON.parse(localStorage.getItem("palettes"))
+
+		savedPalettes = [...paletteObjects]
+
+		paletteObjects.forEach(paletteObj => {
+			//Generate the palette for Library
+			const palette = document.createElement("div")
+
+			palette.classList.add("custom-palette")
+
+			const title = document.createElement("h4")
+
+			title.innerText = paletteObj.name
+
+			const preview = document.createElement("div")
+
+			preview.classList.add("small-preview")
+
+			paletteObj.colors.forEach(smallColor => {
+				const smallDiv = document.createElement("div")
+
+				smallDiv.style.backgroundColor = smallColor
+				preview.appendChild(smallDiv)
+			})
+
+			const paletteBtn = document.createElement("button")
+
+			paletteBtn.classList.add("pick-palette-btn")
+			paletteBtn.classList.add(paletteObj.nr)
+			paletteBtn.innerText = "Select"
+
+			//Attach event to the btn
+			paletteBtn.addEventListener("click", e => {
+				closeLibrary()
+
+				const paletteIndex = e.target.classList[1]
+
+				initialColors = []
+
+				paletteObjects[paletteIndex].colors.forEach((color, index) => {
+					initialColors.push(color)
+					colorDivs[index].style.backgroundColor = color
+					const text = colorDivs[index].children[0]
+					checkTextContrast(color, text)
+					updateHexText(index)
+				})
+
+				resetInput()
+			})
+
+			//Append to Library
+			palette.appendChild(title)
+			palette.appendChild(preview)
+			palette.appendChild(paletteBtn)
+			libraryContainer.children[0].appendChild(palette)
+		})
+	}
+}
+
+getLocal()
 
 randomColors()
